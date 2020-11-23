01da06049dbbb11293e3c7d3f513d516d835162e
island47 <island0318@gmail.com>
2020-11-23 21:04:25 +0900

    Add router

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <span>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+      officia.
+    </span>
+  );
+}
+
+export default About;
